b12c2812e4f694e51d3f465d593da9edc3c45791
20T-yuhj <131419179+20T-yuhj@users.noreply.github.com>
2023-04-21 11:05:24 +0900

    v1.01
    不要なコード削除
    線の太さ変えた

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-file_name = "afterECC-Pt-0.02.txt"
+file_name = "afterECC-Pt-0.02.txt" # 使用するファイル名に書き変える必要あり
 f = open(file_name, 'r').readlines()
 Voltage = []
 Current = []
@@ -15,7 +15,7 @@ start_line = f.index('Potential/V, Current/A\n') + 2
 for lines in f[start_line:]:
     data_temp = lines.split(',')
     Voltage.append(float(data_temp[0]))
-    Current.append(float(data_temp[1]))
+    Current.append(float(data_temp[1])*1000000)
 # データ処理
 
 position = np.where(np.array(Voltage) == initial)[0]
@@ -23,11 +23,10 @@ position = np.where(np.array(Voltage) == initial)[0]
 
 fig,ax1 = plt.subplots()
 
-color = '#0000ff'
 ax1.set_xlabel('Voltage/V')
-ax1.set_ylabel('Current/A')
-ax1.plot(Voltage[int(position[0]):int(position[2]-1)],Current[int(position[0]):int(position[2]-1)],label = '1st',markersize=1)
-ax1.plot(Voltage[int(position[2]):int(position[4]-1)],Current[int(position[2]):int(position[4]-1)],label = '2nd',markersize=1)
-ax1.plot(Voltage[int(position[4]):],Current[int(position[4]):],label = '3rd',markersize=1)
+ax1.set_ylabel('Current/μA')
+ax1.plot(Voltage[int(position[0]):int(position[2]-1)],Current[int(position[0]):int(position[2]-1)],label = '1st',lw=0.75)
+ax1.plot(Voltage[int(position[2]):int(position[4]-1)],Current[int(position[2]):int(position[4]-1)],label = '2nd',lw=0.75)
+ax1.plot(Voltage[int(position[4]):],Current[int(position[4]):],label = '3rd',lw=0.75)
 plt.legend()
 plt.show()
